cb4c5f62163daf44a0804b872edc04fd72caa88e
mckennyjp <jumbomax792@gmail.com>
2022-06-21 15:34:38 -0500

    Gold #11

---
+++ First Platformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int currentGold;
+    public Text goldText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddGold(int goldToAdd)
+    {
+        currentGold += goldToAdd;
+        goldText.text = "Gold: " + currentGold;
+    }
+}
